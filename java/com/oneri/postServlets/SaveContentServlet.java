c2e9f11064d425a7856caabbb5af2cc57cd8ce13
gabrieldemarmiesse <gabrieldemarmiesse@hotmail.fr>
2015-12-03 15:32:43 +0100

    Verification du type d'objet dans le saveContent

--- java/com/oneri/postServlets/SaveContentServlet.java
+++ java/com/oneri/postServlets/SaveContentServlet.java
@@ -30,28 +30,35 @@ public class SaveContentServlet extends javax.servlet.http.HttpServlet{
         String description = req.getParameter("description");
         String commercialLink = req.getParameter("commercialLink");
 
-        // Take a reference of the datastore
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        if(contentType.equals("movie")||contentType.equals("series")&&contentType.equals("comic")
+                ||contentType.equals("video game")||contentType.equals("book")||contentType.equals( "music")){
+            resp.getWriter().println(contentType + " n'est pas un type valide");
+        }else {
 
-        // Generate or retrieve the key associated with an existent contact
-        // Create or modify the entity associated with the contact
-        Entity content;
 
-        //This line means that the title and contentType are assembled to make the key
-        content = new Entity("Content", title + contentType);
-        content.setProperty("Title", title);
-        content.setProperty("ContentType", contentType);
-        content.setProperty("Creator", creator);
-        content.setProperty("ImageURL", imageURL);
-        content.setProperty("Description",description);
-        content.setProperty("CommercialLink",commercialLink);
+            // Take a reference of the datastore
+            DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
-        // Save in the Datastore
-        datastore.put(content);
-        resp.getWriter().println("Content"+ title + " saved with key " +
-                KeyFactory.keyToString(content.getKey()));
+            // Generate or retrieve the key associated with an existent contact
+            // Create or modify the entity associated with the contact
+            Entity content;
 
-        //Go to appengine.google.com to see the DB
+            //This line means that the title and contentType are assembled to make the key
+            content = new Entity("Content", title + contentType);
+            content.setProperty("Title", title);
+            content.setProperty("ContentType", contentType);
+            content.setProperty("Creator", creator);
+            content.setProperty("ImageURL", imageURL);
+            content.setProperty("Description", description);
+            content.setProperty("CommercialLink", commercialLink);
+
+            // Save in the Datastore
+            datastore.put(content);
+            resp.getWriter().println("Content" + title + " saved with key " +
+                    KeyFactory.keyToString(content.getKey()));
+
+            //Go to appengine.google.com to see the DB
+        }
     }
 
 
